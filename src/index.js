fd96ab77d57e0b42262294eadda10279e38e0252
Berget1411 <ludvig.benjaminbergstrom@gmail.com>
2023-12-03 17:55:30 +0100

    add section my-projects

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './styles/main.scss';
+
+const projects = document.querySelectorAll('.project');
+const links = document.querySelectorAll('.project .links img');
+
+const showProject = (e) => {
+  projects.forEach((project) => {
+    if (project.contains(e.target)) return;
+    if (project !== e.target) project.classList.add('hide-project');
+  });
+};
+
+projects.forEach((project) =>
+  project.addEventListener('mouseover', showProject),
+);
+
+const showAllProjects = () => {
+  projects.forEach((project) => project.classList.remove('hide-project'));
+};
+
+projects.forEach((project) =>
+  project.addEventListener('mouseleave', showAllProjects),
+);
--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,7 @@ module.exports = {
     rules: [
       {
         test: /\.html$/i,
+        exclude: /node_modules/,
         loader: 'html-loader',
       },
       {
@@ -58,6 +59,7 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        exclude: /node_modules/,
       },
     ],
   },
